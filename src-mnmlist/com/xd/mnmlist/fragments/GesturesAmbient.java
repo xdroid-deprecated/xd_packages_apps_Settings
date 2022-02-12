4a9819c3721dd5a08ef663568363eff928767f63
xyzuan <xyzuannihboss@gmail.com>
2022-02-12 09:39:31 +0000

    Settings: mnmlist: [SQUASH] Add Feature preferences
    Settings: Add toggle for advanced reboot [2/2]
    @adindotdev
    
    Settings: Double tap to sleep on lockscreen [2/2]
    @ezio84
    
    Settings: Use DT2W toggle for lockscreen DT2S
    @ganeshi4u
    
    Settings: Double tap to sleep on statusbar [2/2]
    @ezio84
    
    Settings: Run statusbar icons from Settings
    @xyyx
    
    Settings: Add clock settings [2/2]
    @kufikugel
    
    Settings: Volume key music control [2/2]
    @maxwen
    
    Settings: Allow doubletap/longpress power to toggle torch [2/2]
    @ezio84
    
    Settings: Add option to launch display customizations fragment
    @imjyotiraditya
    
    Settings: Add monet settings
    @idoybh
    
    Settings: Allow using 4G icon instead LTE [2/2]
    @neobuddy89
    
    Settings: Battery options: cleanup [2/2]
    @ezio84
    
    Settings: Add battery styles and customizations[2/2]
    @TheStrix
    
    Settings: Double tap to trigger doze [2/2]
    @SKULSHADY
    
    Settings: Optional haptic feedback on back gesture [1/2]
    @DennySPB
    
    Settings: Allow to swap volume buttons rotation based [2/2]
    @maxwen
    
    Settings: Allow hiding auto brightness toggle [2/2]
    @idoybh
    
    Settings: Add pref for qs brightness slider position [2/2]
    @idoybh
    
    Settings: add option to enable AOD on charging only [2/2]
    @darkobas
    
    Settings: Allow to configure lockscreen media art blur [2/2]
    @ezio84
    
    Settings: Bring back lockscreen media art [2/2]
    @idoybh
    
    Settings: Ambient Edge Pulse layout [1/2]
    @HerrscherOfDarkness
    
    Settings: Only allow edge lights on AOD when AOD is enabled [2/2]
    @idoybh
    
    Settings: Add ambient edge lightning settings
    @idoybh
    
    Settings: These monet switches should depend
    @idoybh
    
    LockscreenCharging: squashed (3/3)
    @xyyx
    
    Settings: Network Traffic: Allow to choose display mode [1/2]
    @SKULSHADY
    
    Settings: Network Traffic: Customize refresh interval [2/2]
    @neobuddy89
    
    Settings: Cleanup Network traffic
    @SKULSHADY
    
    Settings: Notch friendly network traffic [2/2]
    @SKULSHADY
    
    Settings: Hide arrows in network traffic indicators [2/2]
    @Nico60
    
    Settings: Statusbar network traffic [2/2]
    @ezio84
    
    Settings: Allow forcing small keyguard clock [2/2]
    @idoybh
    
    Settings: Statusbar brightness control [2/2]
    @Danesh
    
    Settings: Allow toggling combined signal icons [2/2]
    @jhonboy121
    @idoybh
    
    co-authored-by: adindotdev <adin@adin.dev>
    co-authored-by: ezio84 <brabus84@gmail.com>
    co-authored-by: ganeshi4u <ganeshvarma@arrowos.net>
    co-authored-by: xyyx <xyyx@mail.ru>
    co-authored-by: kufikugel <kufikugel@users.noreply.github.com>
    co-authored-by: maxwen <max.weninger@gmail.com>
    co-authored-by: imjyotiraditya <jyotiraditya@aospa.co>
    co-authored-by: idoybh <idoybh2@gmail.com>
    co-authored-by: neobuddy89 <neobuddy89@gmail.com>
    co-authored-by: TheStrix <parthbhatia98@gmail.com>
    co-authored-by: SKULSHADY <anushekprasal@gmail.com>
    co-authored-by: DennySPB <dennyspb@gmail.com>
    co-authored-by: darkobas <darkobas@gmail.com>
    co-authored-by: AnkitGourav <Ankitgourav035@gmail.com>
    co-authored-by: Danesh <daneshm90@gmail.com>
    co-authored-by: jhonboy121 <alfredmathew05@gmail.com>

---
+++ src-mnmlist/com/xd/mnmlist/fragments/GesturesAmbient.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2019-2020 The Evolution X Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.xd.mnmlist.fragments;
+
+import android.os.Bundle;
+
+import androidx.preference.Preference;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class GesturesAmbient extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.mnmlist_gestures_ambient);
+
+        getActivity().getActionBar().setTitle(R.string.double_tap_title);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.XD_SETTINGS;
+    }
+}
