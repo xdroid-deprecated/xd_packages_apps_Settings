e32daac2d718b0727bf65a00ed329d07dd2b9264
Pranav Vashi <neobuddy89@gmail.com>
2022-02-12 22:55:13 +0530

    Settings: mnmlist: User toggle for GamesProp [2/2]
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- src-mnmlist/com/xd/mnmlist/fragments/Misc.java
+++ src-mnmlist/com/xd/mnmlist/fragments/Misc.java
@@ -16,10 +16,29 @@
  */
 package com.xd.mnmlist.fragments;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.SystemProperties;
+import android.os.UserHandle;
+import android.provider.Settings;
+import android.text.TextUtils;
+import android.widget.Toast;
 import android.content.Context;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
+
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
 
@@ -27,13 +46,34 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Misc extends SettingsPreferenceFragment {
+public class Misc extends SettingsPreferenceFragment
+                  implements Preference.OnPreferenceChangeListener {
+
+    private static final String KEY_GAMES_SPOOF = "use_games_spoof";
+    private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
+
+    private SwitchPreference mGamesSpoof;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.mnmlist_misc);
+
+        mGamesSpoof = (SwitchPreference) findPreference(KEY_GAMES_SPOOF);
+        mGamesSpoof.setChecked(SystemProperties.getBoolean(SYS_GAMES_SPOOF, false));
+        mGamesSpoof.setOnPreferenceChangeListener(this);
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mGamesSpoof) {
+            boolean value = (Boolean) newValue;
+            SystemProperties.set(SYS_GAMES_SPOOF, value ? "true" : "false");
+            return true;
+        }
+        return false;
     }
 
     @Override
