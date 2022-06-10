234327ef3fc98ac384ee0f46674b65078444b4d3
xyzuan <xyzuannihboss@gmail.com>
2022-06-10 04:12:44 +0000

    Settings: mnmlist: [SQUASH] User toggle for unlimited photos storage
    fixup! Settings: mnmlist: User toggle for unlimited photos storage [2/2]
    
    Co-authored-by: spezi77 <spezi7713@gmx.net>

--- src-mnmlist/com/xd/mnmlist/fragments/Misc.java
+++ src-mnmlist/com/xd/mnmlist/fragments/Misc.java
@@ -50,9 +50,13 @@ public class Misc extends SettingsPreferenceFragment
                   implements Preference.OnPreferenceChangeListener {
 
     private static final String KEY_GAMES_SPOOF = "use_games_spoof";
+    private static final String KEY_PHOTOS_SPOOF = "use_photos_spoof";
+
     private static final String SYS_GAMES_SPOOF = "persist.sys.pixelprops.games";
+    private static final String SYS_PHOTOS_SPOOF = "persist.sys.pixelprops.gphotos";
 
     private SwitchPreference mGamesSpoof;
+    private SwitchPreference mPhotosSpoof;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -64,6 +68,10 @@ public class Misc extends SettingsPreferenceFragment
         mGamesSpoof.setChecked(SystemProperties.getBoolean(SYS_GAMES_SPOOF, false));
         mGamesSpoof.setOnPreferenceChangeListener(this);
 
+        mPhotosSpoof = (SwitchPreference) findPreference(KEY_PHOTOS_SPOOF);
+        mPhotosSpoof.setChecked(SystemProperties.getBoolean(SYS_PHOTOS_SPOOF, true));
+        mPhotosSpoof.setOnPreferenceChangeListener(this);
+
     }
 
     @Override
@@ -71,6 +79,10 @@ public class Misc extends SettingsPreferenceFragment
         if (preference == mGamesSpoof) {
             boolean value = (Boolean) newValue;
             SystemProperties.set(SYS_GAMES_SPOOF, value ? "true" : "false");
+            return true;
+        } else if (preference == mPhotosSpoof) {
+            boolean value = (Boolean) newValue;
+            SystemProperties.set(SYS_PHOTOS_SPOOF, value ? "true" : "false");
             return true;
         }
         return false;
