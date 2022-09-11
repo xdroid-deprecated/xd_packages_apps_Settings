b7df36974ea424c6ec85b317fc9f6ca24f282068
xyzuan <xyzuannihboss@gmail.com>
2022-09-11 02:22:45 +0000

    Settings: xdUI: Override Digital Wellbeing title

--- src/com/android/settings/homepage/TopLevelSettings.java
+++ src/com/android/settings/homepage/TopLevelSettings.java
@@ -228,6 +228,22 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
 
         LayoutPreference myPhone = getPreferenceScreen().findPreference("xd_my_phone");
         myPhone.setLayoutResource(R.layout.xd_dashboard_phone);
+
+        try {
+            Preference digitalWellbeingLegacy = getPreferenceScreen()
+                       .findPreference("top_level_wellbeing");
+            Preference digitalWellbeing = getPreferenceScreen()
+                       .findPreference("dashboard_tile_pref_com.google.android.apps.wellbeing.settings.TopLevelSettingsActivity");
+            Preference digitalWellbeing2 = getPreferenceScreen()
+                       .findPreference("dashboard_tile_pref_com.google.android.apps.wellbeing.home.TopLevelSettingsActivity");
+            if (digitalWellbeingLegacy.isVisible() && digitalWellbeingLegacy.getTitle() != null){
+                digitalWellbeingLegacy.setTitle(R.string.xd_dashboard_wellbeing);
+            } else if (digitalWellbeing.isVisible() && digitalWellbeing.getTitle() != null) {
+                digitalWellbeing.setTitle(R.string.xd_dashboard_wellbeing);
+            } else if (digitalWellbeing2.isVisible() && digitalWellbeing2.getTitle() != null) {
+                digitalWellbeing2.setTitle(R.string.xd_dashboard_wellbeing);
+            }
+        } catch (NullPointerException e) {}
     }
 
     private void initMyPhoneCard(){
