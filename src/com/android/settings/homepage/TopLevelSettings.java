2d8aa1a905cf716eb16ef5f74675a3dfe1eee9a6
xyzuan <xyzuannihboss@gmail.com>
2022-09-12 03:17:43 +0000

    Settings: xdUI: Create mnmlist summary controller [1/2]

--- src/com/android/settings/homepage/TopLevelSettings.java
+++ src/com/android/settings/homepage/TopLevelSettings.java
@@ -71,6 +71,9 @@ import com.android.settingslib.core.instrumentation.MetricsFeatureProvider;
 import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.widget.LayoutPreference;
 
+import android.os.UserHandle;
+import android.provider.Settings;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -191,6 +194,7 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
                     /* scrollNeeded= */ false);
         }
         super.onStart();
+        initMnmlistAdditions();
         initMyPhoneCard();
     }
 
@@ -214,8 +218,21 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
         initPreferenceCard();
     }
 
+    private void initMnmlistAdditions(){
+
+        final boolean mnmlistSummary = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.MNMLIST_SUMMARY, 0, UserHandle.USER_CURRENT) != 0;
+        Preference mnmlistPref = getPreferenceScreen().findPreference("top_level_mnmlist");
+
+        if (!mnmlistSummary){
+            mnmlistPref.setSummary(null);
+        } else {
+            mnmlistPref.setSummary(R.string.mnmlist_summary);
+        }
+    }
+
     private void initPreferenceCard(){
-        
+
         LayoutPreference myPhone = getPreferenceScreen().findPreference("xd_my_phone");
         SwitchPreference switchPref = getPreferenceScreen().findPreference("airplane_mode");
 
