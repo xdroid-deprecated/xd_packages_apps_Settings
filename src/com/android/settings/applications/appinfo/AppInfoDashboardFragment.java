108642c28ea078a60844de55a6d8cdc9dbec7a0d
Asriadi <asriadirahim03@gmail.com>
2023-01-21 08:46:44 +0800

    AppInfo & BatteryUsage: Adapted xdUtils & move batteryusage

--- src/com/android/settings/applications/appinfo/AppInfoDashboardFragment.java
+++ src/com/android/settings/applications/appinfo/AppInfoDashboardFragment.java
@@ -398,7 +398,7 @@ public class AppInfoDashboardFragment extends DashboardFragment
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
         Context mContext = getContext();
-        if (com.android.internal.util.derp.derpUtils.isPackageInstalled(mContext,"com.android.vending")) {
+        if (com.android.internal.util.xd.xdUtils.isPackageInstalled(mContext,"com.android.vending")) {
             menu.add(0, PLAY_STORE, 0, R.string.app_play_store)
                     .setIcon(R.drawable.ic_menu_play_store)
                     .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
@@ -440,7 +440,7 @@ public class AppInfoDashboardFragment extends DashboardFragment
         }
         // Utils.isSystemPackage doesn't include all aosp built apps, like Contacts etc. Add them
         // and grab the Google Play Store itself (com.android.vending) in the process
-        if (com.android.internal.util.derp.derpUtils.isPackageInstalled(getContext(),"com.android.vending")) {
+        if (com.android.internal.util.xd.xdUtils.isPackageInstalled(getContext(),"com.android.vending")) {
             menu.findItem(PLAY_STORE).setVisible(!Utils.isSystemPackage(getContext().getResources(), mPm, mPackageInfo)
                 && !isAospOrStore(mAppEntry.info.packageName));
         }
