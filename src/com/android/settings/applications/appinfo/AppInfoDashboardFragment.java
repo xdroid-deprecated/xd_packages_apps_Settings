ShapeShiftOS/android_packages_apps_Settings

AppInfo: Only add Playstore action if Playstore is installed

src/com/android/settings/applications/appinfo/AppInfoDashboardFragment.java
@@ -365,9 +365,12 @@ public class AppInfoDashboardFragment extends DashboardFragment
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
-        menu.add(0, PLAY_STORE, 0, R.string.app_play_store)
-                .setIcon(R.drawable.ic_menu_play_store)
-                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        Context mContext = getContext();
+        if (com.android.internal.util.ssos.Utils.isPackageInstalled(mContext,"com.android.vending")) {
+            menu.add(0, PLAY_STORE, 0, R.string.app_play_store)
+                    .setIcon(R.drawable.ic_menu_play_store)
+                    .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        }
         menu.add(0, UNINSTALL_UPDATES, 0, R.string.app_factory_reset)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
         menu.add(0, UNINSTALL_ALL_USERS_MENU, 1, R.string.uninstall_all_users_text)

xdroid-deprecated/xd_packages_apps_Settings

Change-Id: Ia558d7d0594b41a9a4510005c3bd5ed1cadc91d6

src/com/android/settings/applications/appinfo/AppInfoDashboardFragment.java
@@ -397,9 +397,12 @@ public class AppInfoDashboardFragment extends DashboardFragment
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
-        menu.add(0, PLAY_STORE, 0, R.string.app_play_store)
-                .setIcon(R.drawable.ic_menu_play_store)
-                .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        Context mContext = getContext();
+        if (com.android.internal.util.derp.derpUtils.isPackageInstalled(mContext,"com.android.vending")) {
+            menu.add(0, PLAY_STORE, 0, R.string.app_play_store)
+                    .setIcon(R.drawable.ic_menu_play_store)
+                    .setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        }
         menu.add(0, UNINSTALL_UPDATES, 0, R.string.app_factory_reset)
                 .setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
         menu.add(0, UNINSTALL_ALL_USERS_MENU, 1, R.string.uninstall_all_users_text)